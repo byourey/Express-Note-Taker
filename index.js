64f00a04e50199e3911d19b1bbbb61571d2dc076
Bjorn Yourey <byourey7783@gmail.com>
2021-03-06 17:48:07 -0500

    updated apiroutes files

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(apiRoutes);
-app.use(htmlRoutes);
+app.use('/', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`App is listening on PORT: ${PORT}`);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,36 @@
 const router = require("express").Router();
 const fs = require("fs");
+const datadb = require("../db/db.json");
 
 const uuid = require("uuid");
 
-const datadb = require("../db/db.json");
 
 router.get('/api/notes', (req, res) => {
-    try {
-        res.json(datadb);
-    } catch (err) {
-        res.status(500).end();
-    }
+    fs.readFile('./db/db.json', (err, data) => {
+        if(err) throw err;
+        const parsedNotes = JSON.parse(data);
+        res.json(parsedNotes);
+    });
+});
+
+router.post('/api/notes', (req, res) => {
+    let noteId = uuid.v4();
+    let newNote = {
+        id: noteId,
+        title: req.body.title,
+        text: req.body.text,
+
+    };
+
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        const parsedNotes = JSON.parse(data);
+        parsedNotes.push(newNote);
+        fs.writeFile('./db/db.json', JSON.stringify(parsedNotes), err => {
+            if (err) throw err;
+            res.send(notes)
+        });
+    });
 });
 
 module.exports = router;
