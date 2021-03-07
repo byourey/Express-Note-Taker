de8c1bc2439a014615cda27bcd1548258152ca06
Bjorn Yourey <byourey7783@gmail.com>
2021-03-07 17:51:38 -0500

    done

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,32 +3,35 @@ const fs = require("fs");
 const dbData = require("../db/db.json");
 
 const uuid = require("uuid");
+const { notStrictEqual } = require("assert");
 
 
 // Get Data
-router.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', (err, data) => {
-        if (err) throw err;
-        const parsedNotes = JSON.parse(data);
-        res.json(parsedNotes);
-    });
+router.get("/api/notes", async (req, res) => {
+    try {
+        res.json(dbData);
+
+    } catch (err) {
+        res.status(500).end();
+     }
 });
 
 // Creating a new note
 router.post('/api/notes', (req, res) => {
+    let noteId = uuid.v4();
     let newNote = {
-        id: uuid4(),
+        id: noteId,
         title: req.body.title,
-        text: req.body.text
+        text: req.body.text,
 
     };
 
     fs.readFile('./db/db.json', (err, data) => {
         if (err) throw err;
-        const getNewNotes = JSON.parse(data);
-        getNewNotes.push(newNote);
+        const parsedNotes = JSON.parse(data);
+        parsedNotes.push(newNote);
 
-        fs.writeFile('./db/db.json', JSON.stringify(getNewNotes), err => {
+        fs.writeFile('./db/db.json', JSON.stringify(parsedNotes), err => {
             if (err) throw err;
             res.send(dbData)
         });
